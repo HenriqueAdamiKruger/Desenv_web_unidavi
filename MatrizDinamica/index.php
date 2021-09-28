f6bc1d86398c83ec6a5e1b73a14a651e24c39c5a
HenriqueAdamiKruger <henrique.adami@unidavi.edu.br>
2021-09-28 13:27:17 -0300

    Finalizando atividade

---
+++ MatrizDinamica/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vetor dinamico</title>
+</head>
+<body>
+    <?php
+        $random = rand(10,30);
+        $matriz = array();
+
+        echo ('<table>');
+
+        for ($i = 0; $i < $random; $i++) { 
+            echo ('<tr style="border: 1px solid black;">');
+            for ($i2 = 0; $i2 < $random; $i2++) { 
+                $matriz[$i2] = rand(0,100);
+                echo ('<td style="border: 1px solid black;">');
+                echo ($matriz[$i2]);
+                echo ('</td>');
+            }
+            echo ('</tr>');
+        }
+        
+        echo ('</table>');
+    ?>
+</body>
+</html>
